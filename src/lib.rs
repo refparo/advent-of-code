5dc63e0f5faa6495c26cc270fb16d51dc4fffbb6
refparo <ref.paro@outlook.com>
2021-12-05 14:53:45 +0800

    reorganize files by week

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,5 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-pub mod day1p1;
-pub mod day1p2;
-
-pub mod day2p1;
-pub mod day2p2;
-
-pub mod day3p1;
-pub mod day3p2;
-
-mod day4;
-pub mod day4p1;
-pub mod day4p2;
-
-mod day5;
-pub mod day5p1;
-pub mod day5p2;
+pub mod week1;
+pub mod week2;
